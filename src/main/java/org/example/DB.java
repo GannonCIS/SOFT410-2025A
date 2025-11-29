7b20bc07cb3c68ab11ac809bf6e30545e1f890f1
Amartuvshin <amartuvshin_g@yahoo.com>
2025-11-29 09:56:10 -0500

    Adjusting Jenkins

--- src/main/java/org/example/DB.java
+++ src/main/java/org/example/DB.java
@@ -7,14 +7,14 @@ public class DB {
     private static final boolean IS_TEST = Boolean.getBoolean("test.env");
 
     public static Connection get() throws Exception {
-        if (IS_TEST) {
+        if ("true".equals(System.getProperty("test.env"))) {
             return DriverManager.getConnection("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1", "sa", "");
         }
-        // Original Postgres connection
         return DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/auth_system",
                 "postgres",
                 "2331"
         );
     }
+
 }
